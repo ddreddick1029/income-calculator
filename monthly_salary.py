18139d0bd264f0be06183426a1881194ea0965d9
ddreddick1029 <reddickd@appstate.edu>
2022-05-06 15:42:34 -0400

    barebones construction of annual salary, monthly income

---
+++ monthly_salary.py
@@ -0,0 +1,5 @@
+print("Please input your annual salary per year: ", end=' ')
+salary = float(input())
+
+monthly_income = (salary / 12)
+print("If you get paid an annual salary of $", salary, "you should receive $", monthly_income, "per month!")
