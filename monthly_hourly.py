6ded09175e151db401dc1b20660ac9070dfb271c
ddreddick1029 <reddickd@appstate.edu>
2022-05-12 16:44:07 -0400

    Need to fix Input to accept no as a value

--- monthly_hourly.py
+++ monthly_hourly.py
@@ -1,13 +1,25 @@
-print("Welcome to the Income Calculator!")
+run = True
 
-print("Please input how much you are expected to make per hour: ", end=' ')
-hourlypay = float(input())
+while run:
 
-print("Please input how many hours you expect to work per week: ", end=' ')
-hoursweek = float(input())
+    print("Welcome to the Income Calculator!")
 
-monthly_income = ((hourlypay * hoursweek) * 52 / 12)
+    print("Please input how much you are expected to make per hour: ", end=' ')
+    hourlypay = float(input())
 
-print(
-    f"If you get paid at a rate of ${hourlypay} and perform {hoursweek} hours of work, you can expect to make "
-    f"${round(monthly_income, 2)} each month!")
+    print("Please input how many hours you expect to work per week: ", end=' ')
+    hoursweek = float(input())
+
+    monthly_income = ((hourlypay * hoursweek) * 52 / 12)
+
+    print(
+        f"If you get paid at a rate of ${hourlypay} and perform {hoursweek} hours of work, you can expect to make "
+        f"${round(monthly_income, 2)} each month!")
+
+    run_again = input("Would you attempt with different values?: ")
+    while True:
+        if run_again == 'yes':
+            run = True
+            continue
+        elif run_again == 'no':
+            break
--- monthly_salary.py
+++ monthly_salary.py
@@ -1,8 +1,21 @@
-print(f"Please input your annual salary per year: ", end=' ')
+run = True
 
-salary = float(input())
+while run:
+    print(f"Please input your annual salary per year: ", end=' ')
 
-monthly_income = (salary / 12)
-print(f"If you get paid an annual salary of ${round(salary, 2)} you should receive ${round(monthly_income, 2)} per "
-      f"month!")
+    salary = float(input())
 
+    monthly_income = (salary / 12)
+    print(f"If you get paid an annual salary of ${round(salary, 2)} you should receive ${round(monthly_income, 2)} per "
+          f"month!")
+
+    run_again = input("Would you attempt with different values?: ").lower()
+    while True:
+        if run_again == 'yes':
+            run = True
+            break
+        elif run_again == 'no':
+            play = False
+            break
+        else:
+            run_again = input('Invalid input! Enter "yes" to continue or "no" to exit the calculator: ')
